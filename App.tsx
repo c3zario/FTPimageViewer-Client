fb1968ecb59193e6b808f5cf8fe4d5216a480380
c3zario <62115336+c3zario@users.noreply.github.com>
2023-05-08 02:30:23 +0200

    Connection to the socket server; downloading thumbnail list; displaying a grid of photos (without scrolling); displaying the selected image in full size and quality

--- App.tsx
+++ App.tsx
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Image, TouchableOpacity, Dimensions } from "react-native";
+import { useState } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { io } from "socket.io-client";
+import { SOCKET_SERVER_URL } from "@env";
+const socket = io.connect(SOCKET_SERVER_URL);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+socket.on("connect", () => {
+    console.log("connect");
+    socket.emit("getThList");
 });
+
+socket.on("disconnect", () => console.log("disconnect"));
+
+socket.on("thList", (list: string[]) => {
+    list.forEach((img: string) => socket.emit("getTh", img));
+});
+
+socket.on("th", (mess: { bytes: string; name: string; ext: string }) => {
+    setImagesList([
+        ...imagesList,
+        { uri: "data:image/jpeg;base64," + mess.bytes, name: mess.name, ext: mess.ext },
+    ]);
+});
+
+let showImgBytes: string;
+socket.on("img", (bytes: string) => {
+    showImgBytes = "data:image/jpeg;base64," + bytes;
+    changeShowImg(!showImg);
+});
+
+let imagesList: any, setImagesList: any, showImg: any, changeShowImg: any;
+export default function App() {
+    const win = Dimensions.get("window");
+
+    [imagesList, setImagesList] = useState([]);
+
+    [showImg, changeShowImg] = useState(false);
+    const showImage = (name: string, ext: string) => {
+        console.log(name);
+        socket.emit("getImg", { name, ext });
+    };
+
+    return (
+        <View style={{ flexDirection: "row", flexWrap: "wrap", height: 500 }}>
+            {showImg == false ? (
+                imagesList.map((image: any, index: number) => (
+                    <TouchableOpacity onPress={() => showImage(image.name, image.ext)} key={index}>
+                        <Image
+                            style={{ width: 96, height: 96, margin: 1 }}
+                            key={index}
+                            source={{ uri: image.uri }}
+                        />
+                    </TouchableOpacity>
+                ))
+            ) : (
+                <TouchableOpacity onPress={() => changeShowImg(!showImg)}>
+                    <Image
+                        style={{
+                            height: win.height,
+                            width: win.width,
+                            margin: 1,
+                        }}
+                        resizeMode={"contain"}
+                        source={{ uri: showImgBytes }}
+                    />
+                </TouchableOpacity>
+            )}
+        </View>
+    );
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# FTPimageViewer-Client
+
+## React Native & Expo Go installation
+```
+npm i -g expo-cli
+expo init project_name
+cd project_name
+npm start
+```
+
+### dotenv problem solve
+```
+npm cache clean --force npm start -- --reset-cache
+```
+or
+```
+react-native start "--reset-cache"
+```
+
+## .env example
+```
+SOCKET_SERVER_URL = http://example.com:3000
+```
